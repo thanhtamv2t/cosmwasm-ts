cf3725f28a46e25c53db48dfc9ae5ec7e1608efe
ThanhTam <thanhtam.v2t@gmail.com>
2022-03-11 14:39:42 +0700

    fix: update

---
+++ cjs/rest/wasm/module.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.contractInfo = void 0;
+function contractInfo(sdk, address) { }
+exports.contractInfo = contractInfo;
